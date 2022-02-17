9d1fd439e0d4ea66b617de81e2c075a760e09830
AndresSalazar <andressalazar.alturo@gmail.com>
2022-02-17 14:23:54 -0500

    Improving urls code

--- mygram_main_folder/users/forms.py
+++ mygram_main_folder/users/forms.py
@@ -2,7 +2,52 @@
 
 #Django
 from operator import truediv
+import profile
 from django import forms
+
+#Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+class SignupForm(forms.Form):
+    """Signup form"""
+    username = forms.CharField(min_length=4, max_length=50)
+
+    password = forms.CharField(max_length=70, widget=forms.PasswordInput())
+    password_confirmation = forms.CharField(max_length=70, widget=forms.PasswordInput())
+
+    first_name = forms.CharField(min_length=2, max_length=50)
+    last_name = forms.CharField(min_length=2, max_length=50)
+
+    email = forms.CharField(min_length=6, max_length=70, widget=forms.EmailInput())
+
+    def clean_username(self):
+        """Username must be unique"""
+        username = self.cleaned_data['username']        ##Con self.cleaned_data accedo a la info
+        username_taken = User.objects.filter(username=username).exists() ##Para saber si existe este Query
+        if username_taken:
+            raise forms.ValidationError('username is already in use')   ##Django eleva el solo el error al template
+        return username
+
+    def clean(self):
+        """Verify password confirmation"""
+        data = super().clean()                ##Con super llama al metodo clean, antes de ser sobre escrito
+
+        password = data['password']
+        password_confirmation = data['password_confirmation']
+
+        if password != password_confirmation:
+            raise forms.ValidationError('Passwords do not match')       #Django eleva solo el error al template
+        return data
+
+    def save(self):
+        """Create user and profile"""
+        data = self.cleaned_data
+        data.pop('password_confirmation')
+
+        user = User.objects.create_user(**data)         ## El **data manda todo el diccionario con las variables
+        profile = Profile(user=user)
+        profile.save()
 
 class ProfileForm(forms.Form):
     """Profile form"""
--- mygram_main_folder/users/views.py
+++ mygram_main_folder/users/views.py
@@ -13,7 +13,7 @@ from users.models import Profile
 from django.db.utils import IntegrityError
 
 #Forms
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 
 # Create your views here.
 
@@ -66,43 +66,57 @@ def login_view(request):
     return render(request, 'users/login.html')
 
 def signup_view(request):
-    """Signup a user"""
+    """Signup Viwe"""
+
     if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else: 
+        form = SignupForm()
+    return render(
+        request=request,
+        template_name='users/signup.html',
+        context={'form' : form}
+    )
 
-        username = request.POST['username']
-        password = request.POST['password']
-        password_confirmation = request.POST['password_confirmation']
-
-        if password != password_confirmation:
-            return render('users/signup.html', {'error': 'Password confirmation does not match'})
-
-        #Elevamos un error en caso de que se intente registrar con un username ya utilizado
-        try:
-            ##Si coinciden las contraseñas, entonces vamos a crear el usuario
-            user = User.objects.create_user(username = username, password = password)
-        except IntegrityError:
-            #Damos un mensaje de error al ya existir ese usuario y ser lo mas especificos posibles en
-            #el error que vamos a tomar, ya que pueden pasar muchos timpos de error o situaciones
-            return render(request, 'users/signup.html', {'error': 'Username already used'})
-
-        # EMAIL VALIDATION
-        user.email = request.POST['email']
-        if User.objects.filter(email=user.email):
-            return render(request, 'users/signup.html', {'error': 'Email is already in used!'})
-
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        
-        user.save()
-
-        #Crear una instancia de profile
-        profile = Profile(user = user)
-        #Siempre salvar los objetos
-        profile.save()
-
-        return redirect('login')
-
-    return render(request, 'users/signup.html')
+    #if request.method == 'POST':
+#
+    #    username = request.POST['username']
+    #    password = request.POST['password']
+    #    password_confirmation = request.POST['password_confirmation']
+#
+    #    if password != password_confirmation:
+    #        return render('users/signup.html', {'error': 'Password confirmation does not match'})
+#
+    #    #Elevamos un error en caso de que se intente registrar con un username ya utilizado
+    #    try:
+    #        ##Si coinciden las contraseñas, entonces vamos a crear el usuario
+    #        user = User.objects.create_user(username = username, password = password)
+    #    except IntegrityError:
+    #        #Damos un mensaje de error al ya existir ese usuario y ser lo mas especificos posibles en
+    #        #el error que vamos a tomar, ya que pueden pasar muchos timpos de error o situaciones
+    #        return render(request, 'users/signup.html', {'error': 'Username already used'})
+#
+    #    # EMAIL VALIDATION
+    #    user.email = request.POST['email']
+    #    if User.objects.filter(email=user.email):
+    #        return render(request, 'users/signup.html', {'error': 'Email is already in used!'})
+#
+    #    user.first_name = request.POST['first_name']
+    #    user.last_name = request.POST['last_name']
+    #    
+    #    user.save()
+#
+    #    #Crear una instancia de profile
+    #    profile = Profile(user = user)
+    #    #Siempre salvar los objetos
+    #    profile.save()
+#
+    #    return redirect('login')
+#
+    #return render(request, 'users/signup.html')
 
 @login_required                          ##Con el decorador, evitamos que haga el logout de una sesion inexistente
 def logout_view(request):
